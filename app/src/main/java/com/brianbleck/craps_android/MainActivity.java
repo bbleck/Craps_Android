8cdd54b88f3ebff470548810e1cc73740241a26a
brianb <bbleck@unm.edu>
2018-10-16 14:37:53 -0600

    Initial commit

---
+++ app/src/main/java/com/brianbleck/craps_android/MainActivity.java
@@ -0,0 +1,13 @@
+package com.brianbleck.craps_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
